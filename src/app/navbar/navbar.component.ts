9d1338fc2cca6422fa301d71cf12e332ab030fea
Sebastien Gadot <>
2018-08-07 14:24:13 +0200

    corrections diverses

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   isNavbarCollapsed=true;
-  images = [  'http://res.cloudinary.com/dgtsw7ufe/image/upload/c_scale,w_1200/c_crop,h_571,w_1200/v1533575072/divers/La_boite_a_envie.jpg',
-              'http://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574043/divers/La_boite_a_envies_porte_monnaie.jpg',
-              'http://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574042/divers/La_boite_a_envies_bracelet.jpg',
-              'http://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574041/divers/La-boite-a-envies-Bracelet-bleu-coeur.jpg',
-              'http://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574041/divers/La-boite-a-envies-Bracelet-etoile.jpg',
-              'http://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574040/divers/La-boite-a-envies-aumoniere-bleu.jpg'
+  images = [  'https://res.cloudinary.com/dgtsw7ufe/image/upload/c_scale,w_1200/c_crop,h_571,w_1200/v1533575072/divers/La_boite_a_envie.jpg',
+              'https://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574043/divers/La_boite_a_envies_porte_monnaie.jpg',
+              'https://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574042/divers/La_boite_a_envies_bracelet.jpg',
+              'https://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574041/divers/La-boite-a-envies-Bracelet-bleu-coeur.jpg',
+              'https://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574041/divers/La-boite-a-envies-Bracelet-etoile.jpg',
+              'https://res.cloudinary.com/dgtsw7ufe/image/upload/c_crop,h_571,w_1200/v1533574040/divers/La-boite-a-envies-aumoniere-bleu.jpg'
            ]
 }
