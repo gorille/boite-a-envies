a743a1b6ccc4906f4656785638796860412895c0
Sebastien Gadot <>
2018-08-07 14:14:00 +0200

    passage de messenger en composant

---
+++ src/app/messenger/messenger.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messenger',
+  templateUrl: './messenger.component.html',
+  styleUrls: ['./messenger.component.css']
+})
+export class MessengerComponent {
+
+}
