b0c8cd91cb9f16199989555073e8f72000e89326
Sebastien Gadot <>
2018-08-31 17:56:31 +0200

    release Prod OK

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //paypalMode: 'production'
-  paypalMode: 'sandbox',
+  paypalMode: 'production',
+  //paypalMode: 'sandbox',
   shippingCost: 1.8,
 };
