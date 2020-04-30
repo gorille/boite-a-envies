7e56b791edb30b5a85462129f3dfdd49e692933a
Sebastien Gadot <>
2020-04-30 15:46:56 +0200

    rectif stock et ameliorations diverses

--- tasks/compileCollections.js
+++ tasks/compileCollections.js
@@ -5,7 +5,7 @@ const path = require('path');
 const options = { type: 'upload',
                   max_results: 500,
                   context: true,
-                  tags: true
+                  tags: false
                 }
 
 cloudinary.v2.api.resources(options, function(error, result){
@@ -17,11 +17,21 @@ cloudinary.v2.api.resources(options, function(error, result){
     result.resources.forEach(image => {
       const product = {}
       const parts = image.public_id.split('/')
-      if (parts.length === 2 && image.context !== undefined && ["bijoux", "bracelet", "montre", "accessoire"].includes(parts[0])) {
+      if (parts.length === 2 && image.context !== undefined && ["boucle", "bracelet", "collier", "accessoire"].includes(parts[0])) {
         product.id = parts[1]
         product.type = parts[0]
+
+        if ( ! image.context.custom.prix ) {
+          throw 'image '+ image.public_id + 'has no price'
+        }
+        if ( ! image.context.custom.caption ) {
+          throw 'image '+ image.public_id + 'has no caption'
+        }
+        if ( ! image.context.custom.alt ) {
+          throw 'image '+ image.public_id + 'has no description'
+        }
+
         product.prix = image.context.custom.prix
-        product.inStock = image.tags.some(tag => tag === 'EnStock')
         
         if (image.context.custom.taille) {
           product.taille = image.context.custom.taille
